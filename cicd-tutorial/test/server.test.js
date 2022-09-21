83867abfca9e1e66e4ca293363f0d2077c2707a1
Prasad Pathak <prasaddpathak@gmail.com>
2022-09-21 16:44:24 -0400

    Basic Vite App

---
+++ cicd-tutorial/test/server.test.js
@@ -0,0 +1,13 @@
+
+// import { setupCounter } from '../counter.js'
+const c = require('../counter')
+
+describe('Check default values', ()=>{
+    it('Check default counter value', ()=>{
+
+        // var el = document.createElement('div')
+        // newEl = c.setupCounter(el) 
+        // expect(newEl.textContent).toBe('count is 0')
+        expect(1).toBe(1)
+    })
+})
